f1abe3aa05f694da2fa0ab174a3bbf56c131508c
laceymalarky <97048468+laceymalarky@users.noreply.github.com>
2023-03-03 10:47:04 -0600

    modified headers/labels

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
-# %%
-import pandas as pd
-import numpy as np
-import streamlit as st
-import plotly.express as px
-
 # %% [markdown]
 # ### Initialization
 
 # %%
-# Load data from url
-url = 'https://www.sports-reference.com/cbb/seasons/men/2023-ratings.html'
+# Load packages
+import pandas as pd
+import numpy as np
+import streamlit as st
+import plotly.express as px
+from datetime import date
+
+# %% [markdown]
+# ### Load data
+
+# %%
+# Web scrape html from url to get NCAA ratings for the current season
+today = date.today()
+year = today.year
+print(year)
+
+url = ('https://www.sports-reference.com/cbb/seasons/men/{}-ratings.html').format(year)
 html = pd.read_html(url, header=0, skiprows=1)
 df = html[0]
 # Deletes repeating headers in content
@@ -50,14 +59,23 @@ def rank_group(rank):
 # Add new col to dataframe
 teamstats['Ranking'] = teamstats['AP_rank'].apply(rank_group)
 
+# %% [markdown]
+# ### Create Streamlit Elements
+
 # %%
-# creating header with an option to filter the data with a checkbox:
+# creating title/header with an option to filter the data with a checkbox:
 # dataset includes all teams but this will let users decide whether they want
 # to see all teams or just those in the AP Poll's top 25
 
-st.header("2022-23 Men's College Basketball Ratings")
+st.title(f"{year-1}-{year} Men's College Basketball Ratings")
 st.markdown("""
-* **Data Source:** https://www.sports-reference.com
+This app performs web scraping of NCAA men's basketball team statistics for the current season.
+* **Data Source:** https://www.sports-reference.com/cbb/
+* **Definitions:**
+    * **Simple Rating System (SRS)** takes into account average point differential and strength of schedule, separated into offensive and defensive components. The rating is denominated in points above/below average, where zero is average.
+    * **Adjusted Rating** is adjusted for strength of opposition. The offensive rating is an estimate of points scored per 100 possessions; defensive rating is an estimate of points allowed per 100 possessions; net rating is an estimate of point differential per 100 possessions.
+
+    *Non-Division I games are excluded from the ratings.*
 """)
 
 ranked = st.checkbox('Filter the page to only include AP Top 25 Teams')
@@ -81,7 +99,7 @@ conference_choice_all.append('All')
 
 # conference_dropdown = st.multiselect('Conference: ', conference_choice_all)
 conference_dropdown = st.selectbox(
-    'Conference: ', conference_choice_all, index=len(conference_choice_all)-1)
+    'Select a Conference: ', conference_choice_all, index=len(conference_choice_all)-1)
 
 # filtering dataset on chosen conference
 if 'All' in conference_dropdown:
@@ -89,7 +107,7 @@ if 'All' in conference_dropdown:
 else:
     filtered_conf = teamstats[teamstats.Conference == conference_dropdown]
 
-st.header('Display Team Stats for Selected Conference(s)')
+st.header('Display Team Statistics')
 st.write('Data Dimensions: ' +
          str(filtered_conf.shape[0]) + ' rows and ' + str(filtered_conf.shape[1]) + ' columns.')
 st.dataframe(filtered_conf)
@@ -100,28 +118,28 @@ st.dataframe(filtered_conf)
 # %%
 st.header('Compare Offensive and Defensive Ratings')
 st.write("""
-###### Analyze offensive and defensive ratings for unranked and top 25 teams
+###### Analyze Simple Rating System (SRS) split into offensive and defensive components for unranked and top 25 teams
 """)
 
 fig1 = px.scatter(teamstats, x="Offensive_SRS", y='Defensive_SRS',
                   hover_data=[teamstats.index], color='Ranking')
 
-fig1.update_layout(
-    title="<b> Simple Rating System (SRS) split into offensive and defensive components</b>")
 st.plotly_chart(fig1)
 
 # %% [markdown]
 # ### Histogram
 
 # %%
-st.header('Compare Ratings by Conference')
-st.write("""
-###### Select multiple conferences to compare
+st.header('How do Conferences Compare?')
+st.markdown("""
+1. Select multiple conferences
+2. Select a statistic
+3. Compare data distributions
 """)
 
 # create multiselect for conferences
-comparison = st.multiselect(
-    'Conferences to compare: ', conference_choice_all, default='All')
+comparison = st.multiselect('Select Conferences: ',
+                            conference_choice_all, default='All')
 
 # filtering dataset on chosen conference
 if 'All' in comparison:
@@ -135,7 +153,7 @@ list_for_hist = ['Points_per_game', 'Opponent_points_per_game', 'Margin_of_victo
                  'SRS', 'Adj_offensive_rating', 'Adj_defensive_rating', 'Adj_net_rating']
 
 # creating selectbox
-choice_for_hist = st.selectbox('Select a Metric', list_for_hist)
+choice_for_hist = st.selectbox('Select a Statistic', list_for_hist)
 
 # plotly histogram by selected metric  split by the choice of conferences in selectbox, ALL by default
 fig2 = px.histogram(filtered_comp, x=choice_for_hist, color='Conference')
@@ -146,24 +164,6 @@ fig2.update_layout(
 
 # embedding into streamlit
 st.plotly_chart(fig2)
-
-# %% [markdown]
-# ### Glossary
-
-# %%
-# Add glossary of terms
-st.text("""
-###### Glossary:
-* Simple Rating System (SRS):
-    * A rating that takes into account average point differential and strength of schedule, separated into offensive and defensive components. The rating is denominated in points above/below average, where zero is average.
-* Adjusted Rating:
-    * A rating adjusted for strength of opposition.
-    * Offensive Rating - an estimate of points scored (for teams) or points produced (for players) per 100 possessions.
-    * Defensive Rating - an estimate of points allowed per 100 possessions.
-    * Net Rating - an estimate of point differential per 100 possessions.
-
-*Non-Division I games are excluded from the ratings.*
-""")
 
 # %%
 # cd git_projects/practicum_sprint4_project
